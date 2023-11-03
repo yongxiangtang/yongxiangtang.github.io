d9b19611714a82bf0a1b3b5e6e4d58671cc92131
Yongxiang Tang <ytang@guidewire.com>
2023-11-03 16:26:25 -0700

    temporaritly disable routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import Navbar from './routes/Navbar';
-import Home from './routes/Home';
-import About from './routes/About';
+// import Home from './routes/Home';
+// import About from './routes/About';
+import Header from './routes/Header';
+import Footer from './routes/Footer';
+import Main from './routes/Main';
 
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
+      <Header />
+      <Main />
+      <Footer />
+      {/* <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
