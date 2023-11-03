89732f71deb9197327b67574dbe9da2587b46dd5
Yongxiang Tang <ytang@guidewire.com>
2023-11-03 15:09:53 -0700

    init react app

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Yongxiang Tang</h1>
+      <p>Software Engineer</p>
+    </header>
+  );
+}
+
+export default Header;
