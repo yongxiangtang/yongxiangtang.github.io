ce0c0c95e96159a755b864e2b2c061e053171cca
Yongxiang Tang <ytang@guidewire.com>
2023-11-03 15:20:54 -0700

    Updates

---
+++ precache-manifest.e0f6e5d89ceb25a1332f03f87465bfb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719ea3ec433a442993d74aa838beb0fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97c8c1fd59221c2b1e6b",
+    "url": "/static/js/2.e8327f2f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.e8327f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15347bf7933b37e390d6",
+    "url": "/static/js/main.00c5c773.chunk.js"
+  },
+  {
+    "revision": "8f0732d7139cf0fe9920",
+    "url": "/static/js/runtime-main.bb8fda8c.js"
+  }
+]);
