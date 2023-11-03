469fc411b70143c61ef588cb4451a67c203f1dfd
Yongxiang Tang <ytang@guidewire.com>
2023-11-03 16:32:58 -0700

    remove routing because it is a single webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-// import Home from './routes/Home';
-// import About from './routes/About';
 import Header from './routes/Header';
 import Footer from './routes/Footer';
 import Main from './routes/Main';
-
-// import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -13,10 +9,6 @@ const App = () => {
       <Header />
       <Main />
       <Footer />
-      {/* <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-      </Routes> */}
     </div>
   );
 }
