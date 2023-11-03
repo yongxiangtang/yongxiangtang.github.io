39c7a0d8c6d1ec61de8e0514f651318cd341b6b7
Yongxiang Tang <ytang@guidewire.com>
2023-11-03 16:13:32 -0700

    Updates

---
+++ precache-manifest.d357f85254d02c8f13d7dee3b5e00709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a49f937b808a742de29047c8e8f85c",
+    "url": "/yongxiangtang.github.io/index.html"
+  },
+  {
+    "revision": "15ec47c426e4dac41b49",
+    "url": "/yongxiangtang.github.io/static/js/2.e1b87822.chunk.js"
+  },
+  {
+    "revision": "bc82e65c6c1fa9f478ad68b699bf76d2",
+    "url": "/yongxiangtang.github.io/static/js/2.e1b87822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0588e0540166002a51",
+    "url": "/yongxiangtang.github.io/static/js/main.731d51b9.chunk.js"
+  },
+  {
+    "revision": "b40f57ce9b2f4a74407c",
+    "url": "/yongxiangtang.github.io/static/js/runtime-main.cace1f05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0f6e5d89ceb25a1332f03f87465bfb2.js"
+  "/yongxiangtang.github.io/precache-manifest.d357f85254d02c8f13d7dee3b5e00709.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yongxiangtang.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
